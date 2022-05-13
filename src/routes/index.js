10b0cd522a8abba32652543e6caf5c8e35783275
Vinicius Littig dos Santos <69813057+ViniciusLittig@users.noreply.github.com>
2022-05-12 22:57:36 -0300

    Iniciando conexão com o banco de dados
    Foi iniciado as configurações com o banco de dados

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,8 +7,6 @@ import Welcome from '../pages/Welcome'
 import SignIn from '../pages/Signin'
 import Register from '../pages/Register'
 import Home from '../pages/Home'
-import PessoaFisica from '../pages/Register/PessoaFisica';
-import PessoaJuridica from '../pages/Register/PessoaJuridica';
 import Profile from '../pages/Profile/index';
 import Schedule from '../pages/Schedule';
 import Consu_Santa_Clara from '../pages/Clinics/Consu_Santa_Clara';
@@ -41,18 +39,6 @@ export default function Routes() {
             <Stack.Screen
                 name='Register'
                 component={Register}
-                options={{ headerShown: false }}
-            />
-
-            <Stack.Screen
-                name='RegisterFisica'
-                component={PessoaFisica}
-                options={{ headerShown: false }}
-            />
-
-            <Stack.Screen
-                name='RegisterJuridica'
-                component={PessoaJuridica}
                 options={{ headerShown: false }}
             />
 
